da4207e5e25f0760212ca904a09c4579fbb82b69
sze chan <sze@hello-sze.com>
2025-05-23 00:35:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ uploaded_files = st.file_uploader(
 for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
     st.write("filename:", uploaded_file.name)
-    st.write(bytes_data)
+    # st.write(bytes_data)
     
     filename = uploaded_file.name
     # filename = 'nationalpost_gam.png'
