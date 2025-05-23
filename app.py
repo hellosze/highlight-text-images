f88dd9adcf2a9e643ec4b7716b87519887f673f3
sze chan <sze@hello-sze.com>
2025-05-23 00:22:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,40 @@
+#inspired by https://stackoverflow.com/questions/54115780/highlighting-specific-text-in-an-image-using-python
+# this script highlights text where the word is optimera and input file is an image, preferrably a screenshot
+import cv2,re
+import pytesseract
 
+
+# filename = 'quotes1.png'
+# filename = 'SCR-20250508-iwpp.png'
+# filename = 'SCR-20250508-iwur.png'
+# filename = 'SCR-20250508-mlfh.png'
+# filename = 'SCR-20250508-mzsk.png'
+filename = 'SCR-20250522-oyfe.png'
+# filename = 'nationalpost_gam.png'
+# text_search = "optimera"
+text_search = "optimera=Z,"
+# text_search = "optimera=Z(,[A-Z]\d+){8}"
+# text_search = "optimera=Z(,[A-Z][A-Z]?\d+)*"
+# text_search = r"ad\-[A-Z][0-9]{6}"
+# text_search = r"ad-\w{6}"
+# text_search = "arn-viafoura"
+# read the image
+img = cv2.imread(filename)
+
+# run tesseract, returning the bounding boxes
+data = pytesseract.image_to_data(img, output_type='dict')
+print(data)
+boxes = len(data['level'])
+
+for i in range(boxes):
+    if re.search(text_search , data['text'][i], re.MULTILINE):
+        print("Found text")
+        overlay = img.copy()
+        (x, y, w, h) = data['left'][i], data['top'][i], data['width'][i], data['height'][i]
+
+        print(x, y, w, h)
+        cv2.rectangle(overlay, (data['left'][i], data['top'][i]), (data['left'][i]+data['width'][i], data['top'][i]+data['height'][i]),(255,255,0), -1)
+        alpha = 0.2  # Transparency factor.
+        # Following line overlays transparent rectangle over the image
+        img = cv2.addWeighted(overlay, alpha, img, 1 - alpha, 0)
+cv2.imwrite("output_232323s.jpg",img)
