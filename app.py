8ca0babc52365e44343fc84b53a597158b81b8b2
sze chan <sze@hello-sze.com>
2025-05-23 00:35:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,40 +12,34 @@ for uploaded_file in uploaded_files:
     st.write("filename:", uploaded_file.name)
     st.write(bytes_data)
     
-# filename = 'quotes1.png'
-# filename = 'SCR-20250508-iwpp.png'
-# filename = 'SCR-20250508-iwur.png'
-# filename = 'SCR-20250508-mlfh.png'
-# filename = 'SCR-20250508-mzsk.png'
-# filename = 'SCR-20250522-oyfe.png'
     filename = uploaded_file.name
-# filename = 'nationalpost_gam.png'
-# text_search = "optimera"
-text_search = "optimera=Z,"
-# text_search = "optimera=Z(,[A-Z]\d+){8}"
-# text_search = "optimera=Z(,[A-Z][A-Z]?\d+)*"
-# text_search = r"ad\-[A-Z][0-9]{6}"
-# text_search = r"ad-\w{6}"
-# text_search = "arn-viafoura"
-# read the image
-img = cv2.imread(filename)
-
-# run tesseract, returning the bounding boxes
-data = pytesseract.image_to_data(img, output_type='dict')
-print(data)
-boxes = len(data['level'])
-
-for i in range(boxes):
-    if re.search(text_search , data['text'][i], re.MULTILINE):
-        print("Found text")
-        overlay = img.copy()
-        (x, y, w, h) = data['left'][i], data['top'][i], data['width'][i], data['height'][i]
-
-        print(x, y, w, h)
-        cv2.rectangle(overlay, (data['left'][i], data['top'][i]), (data['left'][i]+data['width'][i], data['top'][i]+data['height'][i]),(255,255,0), -1)
-        alpha = 0.2  # Transparency factor.
-        # Following line overlays transparent rectangle over the image
-        img = cv2.addWeighted(overlay, alpha, img, 1 - alpha, 0)
-cv2.imwrite("output.jpg",img)
-
-st.image("output.jpg")
+    # filename = 'nationalpost_gam.png'
+    # text_search = "optimera"
+    text_search = "optimera=Z,"
+    # text_search = "optimera=Z(,[A-Z]\d+){8}"
+    # text_search = "optimera=Z(,[A-Z][A-Z]?\d+)*"
+    # text_search = r"ad\-[A-Z][0-9]{6}"
+    # text_search = r"ad-\w{6}"
+    # text_search = "arn-viafoura"
+    # read the image
+    img = cv2.imread(filename)
+    
+    # run tesseract, returning the bounding boxes
+    data = pytesseract.image_to_data(img, output_type='dict')
+    print(data)
+    boxes = len(data['level'])
+    
+    for i in range(boxes):
+        if re.search(text_search , data['text'][i], re.MULTILINE):
+            print("Found text")
+            overlay = img.copy()
+            (x, y, w, h) = data['left'][i], data['top'][i], data['width'][i], data['height'][i]
+    
+            print(x, y, w, h)
+            cv2.rectangle(overlay, (data['left'][i], data['top'][i]), (data['left'][i]+data['width'][i], data['top'][i]+data['height'][i]),(255,255,0), -1)
+            alpha = 0.2  # Transparency factor.
+            # Following line overlays transparent rectangle over the image
+            img = cv2.addWeighted(overlay, alpha, img, 1 - alpha, 0)
+    cv2.imwrite("output.jpg",img)
+    
+    st.image("output.jpg")
